adb9cfb7e1098bef961a3851109c89cc5619848a
jean.laneurit <jean.laneurit@inrae.fr>
2024-03-30 16:58:14 +0100

    bringup: fix unit test
    Signed-off-by: jean.laneurit <jean.laneurit@inrae.fr>

--- romea_imu_bringup/test/test_imu_bringup.py
+++ romea_imu_bringup/test/test_imu_bringup.py
@@ -38,7 +38,7 @@ def test_has_driver_configuration(meta_description):
 
 
 def test_get_driver_package(meta_description):
-    assert meta_description.get_driver_pkg() == "xsens_driver"
+    assert meta_description.get_driver_package() == "xsens_driver"
 
 
 def test_get_driver_executable(meta_description):
